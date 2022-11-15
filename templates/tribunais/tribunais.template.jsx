3fcdb1975bb016a0ca0e21bb6068c2242f482d97
EnioSFilho <eniofilho76@gmail.com>
2022-11-15 13:13:03 -0300

    feat/tribunais start

---
+++ templates/tribunais/tribunais.template.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
+
+function TribunaisTemplate() {
+  return (
+    <>
+    <Header/>
+    <h1>Teste Tribunais</h1>
+    <Footer/>
+
+    </>
+ )
+}
+
+export default TribunaisTemplate
