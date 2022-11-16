085e80e690620a0b459740d6c837ee5b9d3a9cab
EnioSFilho <eniofilho76@gmail.com>
2022-11-16 09:27:16 -0300

    feat/localização em construção e definição de endereço

--- components/localizacao/Localizacao.jsx
+++ components/localizacao/Localizacao.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 function Localizacao() {
   return (
     <>
-    <div>
-    <iframe style={{width: "100%", height:"100%"}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14636.291995518492!2d-46.62351021158759!3d-23.493880014324038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef6396643a5bf%3A0x11df006ea2de62e8!2sMaria%20Let%C3%ADcia%20B.%20Marques%20Advocacia!5e0!3m2!1spt-BR!2sbr!4v1668595192794!5m2!1spt-BR!2sbr" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <h1 style={{textAlign: "center"}}>Localização</h1>
+    <container className="container-localizacao">
+    <div className='localizacao-mapa'>
+    <iframe style={{width: "80%", height:"200%"}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14636.291995518492!2d-46.62351021158759!3d-23.493880014324038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef6396643a5bf%3A0x11df006ea2de62e8!2sMaria%20Let%C3%ADcia%20B.%20Marques%20Advocacia!5e0!3m2!1spt-BR!2sbr!4v1668595192794!5m2!1spt-BR!2sbr" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
+    <div>
+      <h4>Av. Leôncio de Magalhães, 1100 - Jardim São Paulo - São Paulo-SP - CEP: 02042-011</h4>
+    </div>
+    <div>
+      <h5>Ao lado do metrô Jardim São Paulo</h5>
+    </div>
+    <div className='localizacao-button'>
+    <Button variant='outline-primary' href='https://g.page/r/Cehi3qJuAN8REAI/review'>Avalie Nosso Escritório</Button>
+    </div>
+    </container>
     </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/Footer/index.css"
 import "../styles/Main/index.css"
 import "../styles/Areas/index.css"
 import "../styles/Tribunais/index.css"
+import "../styles/Localizacao/index.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
 
