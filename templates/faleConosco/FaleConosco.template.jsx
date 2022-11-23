ddc50dd1bf80eb1c473cbd883daa83e6a5318da5
EnioSFilho <eniofilho76@gmail.com>
2022-11-23 14:07:51 -0300

    feat/faleconosco razoavel

---
+++ templates/faleConosco/FaleConosco.template.jsx
@@ -0,0 +1,18 @@
+import Footer from '../../components/footer/Footer'
+import Fale, { sendEmail } from '../../components/faleConosco/FaleConosco'
+import Header from '../../components/header/Header'
+import FaleConosco from '../../components/faleConosco/FaleConosco'
+
+
+
+function FaleConoscoTemplate() {
+  return (
+   <>
+   <Header/>
+   <FaleConosco/>
+   <Footer/>
+   </>
+  )
+}
+
+export default FaleConoscoTemplate
