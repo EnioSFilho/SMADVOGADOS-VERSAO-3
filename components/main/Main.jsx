335473343c984a60de4efefbb8b9b4332dda9df1
EnioSFilho <eniofilho76@gmail.com>
2023-08-21 21:00:20 -0300

    trabalhando na main

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -4,11 +4,28 @@ import { ConstainerStyled } from './main.styled'
 
 function Main() {
   return (
-    <div className='adjust'>
+    <div className='containerMain'>
 
-      <container className="puff-in-center">
-        <h5>O nosso escritório de advocacia tem como principal objetivo prestar serviços e desempenhar suas funções com qualidade, sempre preocupado em superar as expectativas do cliente. Atuamos nos âmbitos de direito privado e público, ofertamos a clientes e empresas, de diversos segmentos, advocacia preventiva, serviços de consultoria jurídica, patrocínio de demandas na esfera administrativa ou judicial, em todas as instâncias. Nossas áreas de trabalho abrangem os ramos do Direito Civil, do Direito Penal, do Direito Trabalhista e do Direito Previdenciário e Direito Tributário. Nosso maior compromisso é sempre atender as necessidades dos clientes e prestar o serviço jurídico com qualidade, mantendo nossos clientes informados sobre suas demandas ou solicitações, com o propósito de fomentar a confiabilidade, de forma em que a ética e a responsabilidade social sempre prevaleçam.</h5>
-      </container>
+      <div className='containerInternalMain'>
+
+        <div className='containerText'>
+          <div>
+          <h1>Converse com nossos advogados on-line, para que possamos lhe auxiliar em sua aposentadoria ou benefício previdenciário.</h1>
+          </div>
+          <div>
+          <p>Trabalhamos com profissionais especializados em direito previdenciário,<br></br>
+             para que você tenha a melhor assessoria.</p>
+             </div>
+             
+    <div>
+      <a>Fale com um Profissional</a>
+    </div>
+        </div>
+
+
+
+      </div>
+      
 
     </div>
   )
