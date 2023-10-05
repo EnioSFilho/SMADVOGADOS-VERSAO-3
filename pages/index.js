ab133fff1b0201789343d518e105de01d344a70d
EnioSFilho <eniofilho76@gmail.com>
2023-10-05 15:49:28 -0300

    tags

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Advocacia Previdenciária</title>
-        <meta name='Aposentadoria' content='Aposentadoria por Idade, Revisão de Aposentadoria, Benefício previdenciário, Auxílio Acidente, Auxílio doença, Loas, Revisão da Vida Toda, Aposentadoria'/>     
+        <meta name='keywords' content='Aposentadoria por Idade, Revisão de Aposentadoria, Benefício previdenciário, Auxílio Acidente, Auxílio doença, Loas, Revisão da Vida Toda, Aposentadoria'/>     
         <meta name='description' content='Escritório de Advocacia especializado em direito Previdenciário. Somos especialistas em todos os tipos de ação junto a previdência'/>
       </Head>
       <HomeTemplate />
